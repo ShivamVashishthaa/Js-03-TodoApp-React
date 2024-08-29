60d1afe263aa843bf6258be6dfb9fc3ddc4c1d44
Shivam Vashishtha <shivam283097@gmail.com>
2024-08-29 14:10:52 +0530

    first commit

---
+++ src/components/todo/api/HelloWorldApiService.js
@@ -0,0 +1,31 @@
+import { apiClient } from './ApiClient'
+
+// export function retrieveHelloWorldBean(){
+//     return axios.get('http://localhost:8080/hello-world-bean')
+// }
+
+export const retrieveHelloWorldBean 
+    = () => apiClient.get('/hello-world-bean')
+
+
+//Response to preflight request doesn't pass access control check => Authorization header
+export const retrieveHelloWorldPathVariable
+    = (username, token) => apiClient.get(`/hello-world/path-variable/${username}`
+    // ,{
+    //     headers: {
+    //         Authorization: token
+    //     }
+    // }
+    )
+
+export const executeBasicAuthenticationService
+    = (token) => apiClient.get(`/basicauth`
+    ,{
+        headers: {
+            Authorization: token
+        }
+    }
+    )
+
+
+    
